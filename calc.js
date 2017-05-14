fa8ba133b8bf97ae52b99dedc8a9e99f3e7ef407
AleeWeb <aalexanderpr@gmail.com>
2017-05-13 23:50:45 -0400

    Completed JS Code
    Final JS Code. All done!

--- calc.js
+++ calc.js
@@ -1,5 +1,4 @@
 
-
 var box = document.getElementById("displayScreen");
 
 function addtoScreen(x) { //x is used as a temporary substitute for all the button onclick parameters.
@@ -8,4 +7,26 @@ function addtoScreen(x) { //x is used as a temporary substitute for all the butt
   if(x==="C") {
     box.value=""; //For the CLEAR Button to clear to empty string, display nothing
   }
+}
+
+//For the = sign button, the onclick function name is answer.
+function answer() {
+  x = box.value;
+  x = eval(x);  //Evaluate String. Simply means do the math for each button string parameter.
+  box.value = x;
+}
+
+//For the exponent button x^2
+function power(){
+  x = box.value;
+  x = eval(x*x);
+  box.value = x;
+}
+
+//For the CE button
+function backspace() {
+  var num = box.value;  //Stored the entire string in a new varible.
+  var len = num.length - 1; // Length propery of that strength - 1. No matter the length, one less character
+  var newNum = num.substring(0, len);
+  box.value = newNum;
 }
